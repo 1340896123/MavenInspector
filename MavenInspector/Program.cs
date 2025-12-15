097a6f6dffe01e28e971c55978669d4aa9cd2ed1
廖宇杰 <1340896123@qq.com>
2025-12-15 17:02:50 +0800

    基本可用2

--- MavenInspector/Program.cs
+++ MavenInspector/Program.cs
@@ -6,10 +6,10 @@ var builder = Host.CreateApplicationBuilder(args);
 // 配置日志输出到stderr (stdout用于MCP协议消息)
 builder.Logging.AddConsole(o => o.LogToStandardErrorThreshold = LogLevel.Trace);
 
-var ll=new MavenInspectorTools();
-var dddwwtw = await ll.AnalyzePomDependencies("E:\\TDT\\项目\\沈阳新松半导体\\erdcloud-xspdm-plugin\\pom.xml");
+//var ll=new MavenInspectorTools();
+//var dddwwtw = await ll.AnalyzePomDependencies("E:\\TDT\\项目\\沈阳新松半导体\\erdcloud-xspdm-plugin\\pom.xml");
 
-Console.WriteLine(dddwwtw.ToString()); ;
+//Console.WriteLine(dddwwtw.ToString()); ;
 
 
 // 添加MCP服务：使用stdio传输模式并注册工具
